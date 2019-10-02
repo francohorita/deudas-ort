bab94504af29ec2a0e7063fff303e1ccf6725865
Brian Wider <brian.wider@mercadolibre.com.ar>
2019-10-02 20:12:09 -0300

    Retrieve contacts list

--- app/src/main/java/com/example/deudas_ort/NativeContentProvider.java
+++ app/src/main/java/com/example/deudas_ort/NativeContentProvider.java
@@ -5,23 +5,25 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
-import android.content.OperationApplicationException;
 import android.database.Cursor;
 
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.os.Build;
 import android.provider.ContactsContract;
 
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ListView;
 import android.widget.Button;
+import android.widget.ArrayAdapter;
+import java.util.List;
 import android.widget.Toast;
 
 public class NativeContentProvider extends Activity {
+    private ListView lstNames;
+
     // Request code for READ_CONTACTS. It can be any number > 0.
     private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
 
@@ -32,6 +34,10 @@ public class NativeContentProvider extends Activity {
         setContentView(R.layout.nativecontentlayout);
 
         Button view = (Button)findViewById(R.id.viewButton);
+        super.onCreate(savedInstanceState);
+
+        // Find the list view
+        this.lstNames = (ListView) findViewById(R.id.lstNames);
 
         view.setOnClickListener(new OnClickListener() {
             public void onClick(View v){
@@ -47,28 +53,37 @@ public class NativeContentProvider extends Activity {
             requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
             //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
         } else {
-            ContentResolver cr = getContentResolver();
-            Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                    null, null, null, null);
-            if (cur.getCount() > 0) {
-                while (cur.moveToNext()) {
-                    String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-                    String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                    if (Integer.parseInt(cur.getString(
-                            cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-                        Cursor pCur = cr.query(
-                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                                null,
-                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                                new String[]{id}, null);
-                        while (pCur.moveToNext()) {
-                            String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            Toast.makeText(NativeContentProvider.this, "Name: " + name + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
-                        }
-                        pCur.close();
-                    }
-                }
-            }
+            // Android version is lesser than 6.0 or the permission is already granted.
+            List<String> contacts = getContactNames();
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
+            lstNames.setAdapter(adapter);
         }
+    }
+
+    /**
+     * Read the name of all the contacts.
+     *
+     * @return a list of names.
+     */
+    private List<String> getContactNames() {
+        List<String> contacts = new ArrayList<>();
+        // Get the ContentResolver
+        ContentResolver cr = getContentResolver();
+        // Get the Cursor of all the contacts
+        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+        // Move the cursor to first. Also check whether the cursor is empty or not.
+        if (cursor.moveToFirst()) {
+            // Iterate through the cursor
+            do {
+                // Get the contacts name
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                contacts.add(name);
+            } while (cursor.moveToNext());
+        }
+        // Close the cursor
+        cursor.close();
+
+        return contacts;
     }
 }
